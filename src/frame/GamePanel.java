d3d9fe1eab1f363830bc1606f3cf303d17b19da6
DanL3al <standingunderneath@gmail.com>
2025-09-10 08:30:21 -0300

    feat: add fps

--- src/frame/GamePanel.java
+++ src/frame/GamePanel.java
@@ -3,13 +3,40 @@ package frame;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
+
+    private Thread thread;
 
     public GamePanel(int width, int height){
         this.setBounds(0,0,width,height);
         this.setBackground(Color.green);
     }
 
-    public void startGameThread(){}
+    public void startGameThread(){
+        this.thread = new Thread(this);
+        thread.start();
+    }
 
+    @Override
+    public void run() {
+        int FPS = 60;
+        double drawInterval = 1000000000f/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        while (thread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update(){}
+
+    public void paintComponent(Graphics g){}
 }
